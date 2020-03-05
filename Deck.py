ebe255d7fa9022176942fcc151752afa72ed5194
pavel_chub <49043051+pavelchub1997@users.noreply.github.com>
2020-03-05 23:26:50 +0300

    Update Deck.py

--- Deck.py
+++ Deck.py
@@ -1,15 +1,19 @@
 #The module allows you to mix cards and select N cards
 
 import random
+import result_work
+
 
 class Deck:
     def shuffle_card(self, lst_card):
         shuffle_lst = []
-        while lst_card != []:
+        while len(lst_card) != len(shuffle_lst):
             ind = random.randint(0, len(lst_card)-1)
             shuffle_lst.append(lst_card[ind])
-            lst_card.pop(ind)
         return shuffle_lst
 
-    def get_card(self, N, shuffle_card):
-        return [shuffle_card[ind] for ind in range(N)], shuffle_card[N:]
+    def get_card(self, value_n, shuffle_card):
+        return [shuffle_card[ind] for ind in range(value_n)], shuffle_card[value_n:]
+
+    def get_cards_from_template(self, template, list_cards):
+        return result_work.pattern_search(template, list_cards)
